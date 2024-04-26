65778ad8d534fa39e154d10d0d286a08bc091426
sharkness <samantha.gray@sealights.io>
2024-04-26 14:11:38 -0500

    initial commit

---
+++ src/main/java/com/sldemo/App.java
@@ -0,0 +1,14 @@
+package com.sldemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
